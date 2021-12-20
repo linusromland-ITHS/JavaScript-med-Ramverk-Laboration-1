e3559f9ab5710bb4cf0ff722f9248dd7859068b5
Linus Romland <hello@linusromland.com>
2021-12-20 13:57:08 +0100

    Uppgift: v-for med objekt

---
+++ Vue.js - Villkorlig rendering (v-if) och listrendering (v-for)/v-for med objekt/index.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+	data() {
+		return {
+			products: {
+				'dd3ce0a3-1d30-4e3e-bcec-7095590019d9': {
+					name: 'Jacket',
+					price: 200,
+					quantity: 0
+				},
+				'faf68d1f-8f3e-4668-891c-56118cc330f4': {
+					name: 'Pants',
+					price: 100,
+					quantity: 0
+				},
+				'dd2bb41e-7f0a-4cb8-884b-588f9457dd54': {
+					name: 'Shoes',
+					price: 100,
+					quantity: 0
+				}
+			}
+		};
+	}
+}).mount('#app');
