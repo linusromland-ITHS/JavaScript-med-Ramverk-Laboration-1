ab4947d6815a4e14b139ed91da60b47ca90b2154
Linus Romland <hello@linusromland.com>
2022-01-04 10:52:16 +0100

    Uppgift: Tre webbsidor

---
+++ Vue.js - Vue Router/Tre webbsidor/index.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({});
+
+const index = { template: '<h1>Home</h1>' };
+const p1 = { template: '<h1>Page 1</h1>' };
+const p2 = { template: '<h1>Page 2</h1>' };
+
+const routes = [
+	{
+		component: p1,
+		path: '/page-1'
+	},
+	{
+		component: p2,
+		path: '/page-2'
+	},
+	{
+		component: index,
+		path: '/'
+	}
+];
+
+const router = VueRouter.createRouter({
+	history: VueRouter.createWebHashHistory(),
+	routes: routes
+});
+
+app.use(router);
+
+app.mount('#app');
