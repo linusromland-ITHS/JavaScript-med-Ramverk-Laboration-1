5ad85caed0f67c1fa5ab52dbcb2ae393348f0a9d
Linus Romland <hello@linusromland.com>
2022-01-04 10:28:12 +0100

    Uppgift: En sträng-payload från ett textfält

---
+++ Vue.js - Custom events/En sträng-payload från ett textfält/index.js
@@ -0,0 +1,14 @@
+app.component('controls', {
+	template: '<input v-model="textField"></input><input type="button" @click="onClick">Click me</input>',
+	data() {
+		return {
+			textField: ''
+		};
+	},
+	emits: ['start-game'],
+	methods: {
+		onClick() {
+			this.$emit('start-game', this.textField);
+		}
+	}
+});
